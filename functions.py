2ee1af5f9981815084324582f3edf05178ad2f0a
RonaldKwann <14719@coderacademy.edu.au>
2023-12-12 19:17:22 +1100

    User Menu updated

--- functions.py
+++ functions.py
@@ -11,6 +11,7 @@ def new_user(file_user):
     with open(file_user, "a") as f:
         writer = csv.writer(f)
         writer.writerow([newuser_name, newuser_pwd])
+    print(f"{fg('black')}{bg('white')}Welcome {newuser_name}! \nChoose option 2 to login.{attr('reset')}")
 
 def user(file_user):
     show_menu = True
@@ -25,39 +26,48 @@ def user(file_user):
 
         if user_choice == "1":
             print("You entered 1")
+            print(f"{fg('black')}{bg('white')}Current Passwords{attr('reset')}")
+            view_pwd = input("Please type in your User Password: ")
         elif user_choice == "2":
             print(f"{fg('black')}{bg('white')}Add a Password {attr('reset')}")
-            print("Would you like to add a password manually? Press 1")
-            print("Would you like the system to generate one? Press 2")
-            add_pwd = input("Enter your selection: ")
-            if add_pwd == "1": 
-                manual_username = input("Please create username for password: ")
-                manual_pwd = input(f"Please type in password for {manual_username}:  ")    
-                print(f"{fg('black')}{bg('white')}Password added!{attr('reset')}")
+            while True:
+                print("1. Press 1 to add a password manually")
+                print("2. Press 2 to generate a secure password")
+                print("3. Press 3 to go back to Main menu")
+                add_pwd = input("Enter your selection: ")
+                if add_pwd == "1": 
+                    manual_username = input("Please create username for password: ")
+                    manual_pwd = input(f"Please type in password for {manual_username}:  ")    
+                    print(f"{fg('black')}{bg('white')}{manual_username}'s Password added!{attr('reset')}")
+                    break
+                elif add_pwd == "2":
+                    print(f"{fg('black')}{bg('white')}Generate a Password{attr('reset')}")
+                    gen_username = input("Please create username for password: ")
+                    pwd_gen = input(f"Would you like to generate a Password for {gen_username}? Y/N:  ")
+                    if pwd_gen == "Y":
+                        print(f"Password Generated for {gen_username}:  ")
+                        print("Would you like to save this password? Option 1")
+                        print("Would you like a different generated password? Option 2")
+                        gen_input = input("Enter your selection: ")
+                        break
+                    elif pwd_gen == "N":
+                        print(f"{fg('black')}{bg('white')}User Menu{attr('reset')}")
+                        break
+                    else:
+                        print("Invalid selection! Please enter Y/N")
+                elif add_pwd == "3":
+                    print(f"{fg('black')}{bg('white')}User Menu{attr('reset')}")
+                    break
+                else:
+                    print("Invalid selection! Please enter 1, 2, 3")
+
         elif user_choice == "3":
             print("You entered 3")
-            print("Please type the username of password to delete: ")
+            print(f"{fg('black')}{bg('white')}Delete a Password{attr('reset')}")
+            pwd_delete = input("Please type the username of password to delete: ")
         elif user_choice == "4":
             print(f"{fg('black')}{bg('white')}Main Menu {attr('reset')}")
             show_menu = False
 
-
-# def validate_user(file_path):
-#     input_username = input("Enter your username: ")
-#     input_password = input("Enter your password: ")
-    
-#     with open(file_path, mode='r') as file:
-#         csv_reader = csv.reader(file)
-#         for row in csv_reader:
-#             if row and len(row) >= 2 and row[0] == input_username and row[1] == input_password:
-#                 return True
-#     return False
-
-# file_user = "users.csv"
-
-# if validate_user(file_user):
-#     print("Validation successful. User found in the CSV file.")
-# else:
-#     print("Validation failed. User not found or incorrect credentials.")
 
 
